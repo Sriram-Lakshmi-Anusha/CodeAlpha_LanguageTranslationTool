b16963c604a292b7c255f53811865fa9438d02b5
Sriram-Lakshmi-Anusha <sreeramanjaneyulu312@gmail.com>
2026-06-24 05:54:21 +0530

    CodeAlpha Language Translation Tool

---
+++ README.md
@@ -0,0 +1,46 @@
+# CodeAlpha Language Translation Tool
+
+A simple Language Translation Tool built using Python Flask and Deep Translator.
+
+## Features
+
+- Translate text between multiple languages
+- Clean user interface
+- Fast translation
+- Beginner friendly
+
+## Technologies Used
+
+- Python
+- Flask
+- Deep Translator
+- HTML
+- CSS
+
+## Installation
+
+Install packages:
+
+```bash
+py -m pip install -r requirements.txt
+```
+
+Run:
+
+```bash
+py app.py
+```
+
+Open:
+
+```text
+http://127.0.0.1:5000
+```
+
+## Example
+
+Input:
+Hello
+
+Output:
+నమస్కారం
---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from deep_translator import GoogleTranslator
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    translated = ""
+
+    if request.method == "POST":
+
+        text = request.form["text"]
+
+        source = request.form["source"]
+
+        target = request.form["target"]
+
+        try:
+
+            translated = GoogleTranslator(
+                source=source,
+                target=target
+            ).translate(text)
+
+        except:
+
+            translated = "Translation Failed"
+
+    return render_template(
+        "index.html",
+        translated=translated
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
